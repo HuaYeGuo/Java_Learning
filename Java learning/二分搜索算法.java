3493f0c7c84ab3bbc712def60f0c47f282fe6559
HuaYeGuo <125035762+HuaYeGuo@users.noreply.github.com>
2023-07-15 16:52:01 +0800

    Update 二分搜索算法.java

--- Java learning/二分搜索算法.java
+++ Java learning/二分搜索算法.java
@@ -1,4 +1,4 @@
-/*二分搜索算法*/
+/*二分搜索算法.非递归算法实现*/
 public class Binary_Search{
     public static void main(String[] args) {
         int[] arr = {1, 3, 4, 6, 7, 8, 10, 11, 13, 15};
@@ -14,6 +14,25 @@ public class Binary_Search{
                 right = mid - 1;
             else
                 return mid;
+        }
+        return -1;
+    }
+}
+——————————————————————————————————————————————————————————————————
+/*二分搜索算法.递归算法实现*/
+public class Binary_Search{
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 4, 6, 7, 8, 10, 11, 13, 15};
+        System.out.println(Search(3,arr,0, arr.length - 1));
+    }
+    public static int Search(int target,int[]arr,int left,int right) {
+        int mid = (left + right)/2;
+        if (target == arr[mid]) {
+            return mid;
+        } else if (target < arr[mid]) {
+            return Search(target,arr,left,mid - 1);
+        } else if (target > arr[mid]) {
+            return Search(target,arr,mid + 1,right);
         }
         return -1;
     }
